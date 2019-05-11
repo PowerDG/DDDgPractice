58b48ee92d0af9da9c7778a2ce40f3bb7261bce8
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-11 16:03:11 +0800

    Create SysConfig.cs

---
+++ Dg.ERM/4.6.0RF/aspnet-core/Dg.Core/SysConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dg.Core
+{
+    /// <summary>
+    /// 配置表
+    /// </summary>
+    [Table("SysConfig")]
+    public class SysConfig : AggregateRoot
+    {
+        /// <summary>
+        /// 配置Key
+        /// </summary>
+        public string ConKey { get; set; }
+
+        /// <summary>
+        /// 配置值，Json格式
+        /// </summary>
+        public string ConValue { get; set; }
+
+        /// <summary>
+        /// 配置名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 值类型，int、long等
+        /// </summary>
+        public SysConfigType ConfigType { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 配置分类：网站配置、上传配置等
+        /// </summary>
+        public SysConfigClassify ConfigClassify { get; set; }
+    }
+}
