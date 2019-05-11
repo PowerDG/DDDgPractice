8f789c18aca53c2d4b4b2ca85de7857497f71130
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-11 16:03:06 +0800

    Create AggregateRoot.cs

---
+++ Dg.ERM/4.6.0RF/aspnet-core/Dg.Core/AggregateRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Dg.Core
+{
+    public interface IAggregateRoot
+    {
+        long Id { get; set; }
+
+        long CreateUserId { get; set; }
+        string CreateUserName { get; set; }
+
+        DateTime UpdateTime { get; set; }
+    }
+    /// <summary>
+    /// Asp.Net DDD架构浅谈——整体框架说明
+    /// https://www.jianshu.com/p/c9b28468015e
+    /// 
+    /// Asp.Net DDD架构浅谈——领域划分、仓储应用、Services层定义
+    /// https://www.jianshu.com/p/c0a8b6447d29
+    /// 
+    /// Asp.Net DDD架构浅谈——依赖注入Autofac
+    /// https://www.jianshu.com/p/f5d6346b0a7b
+    /// 
+    /// Asp.Net DDD架构浅谈——网站配置
+    /// https://www.jianshu.com/p/37c906f88f8a
+    /// </summary>
+    public class AggregateRoot
+    {
+
+    }
+}
