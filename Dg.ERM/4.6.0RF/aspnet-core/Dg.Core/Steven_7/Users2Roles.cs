696ab9535342c6e16384d7e035885a78c8cb0062
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-11 16:07:15 +0800

    Move

---
+++ Dg.ERM/4.6.0RF/aspnet-core/Dg.Core/Steven_7/Users2Roles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dg.Core
+{
+    public class Users : AggregateRoot
+    {
+        public Users()
+        {
+            UsersRoles = new HashSet<UsersRoles>();
+        }
+
+        [StringLength(50)]
+        public string Realname { get; set; }
+
+        [StringLength(50)]
+        public string UserName { get; set; }
+    }
+
+    public class UsersRoles : AggregateRoot
+    {
+        public UsersRoles()
+        {
+            Users = new HashSet<Users>();
+        }
+
+        [Display(Name = "角色名")]
+        [Required(ErrorMessage = "请填写“{0}”")]
+        [StringLength(50, ErrorMessage = "{0}最多{1}个字符")]
+        public string Name { get; set; }
+
+    }
+    [Table("User2Role")]
+    public class User2Role : AggregateRoot
+    {
+        public long UserId
+        {
+            get;
+            set;
+        }
+
+        public long RoleId
+        {
+            get;
+            set;
+        }
+
+    }
+}
+                                
