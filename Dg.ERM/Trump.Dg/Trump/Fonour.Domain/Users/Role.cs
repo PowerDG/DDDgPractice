e4d41efaab022e7a98431f32d4ba31d0a58c7da2
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-12 22:42:55 +0800

    Create Role.cs

---
+++ Dg.ERM/Trump.Dg/Trump/Fonour.Domain/Users/Role.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fonour.Domain.Users
+{
+    public class Role : Entity
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        //public Guid CreateUserId { get; set; }
+
+        public DateTime? CreateTime { get; set; }
+
+        public string Remarks { get; set; }
+
+        //public virtual User CreateUser { get; set; }
+
+        //public virtual ICollection<User> Users { get; set; }
+
+        //public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
