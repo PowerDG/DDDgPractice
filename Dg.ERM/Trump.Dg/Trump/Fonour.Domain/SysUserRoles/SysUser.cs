ba8209c34bbb56a5b1fbb5ec8277bf06a7481529
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-12 22:42:40 +0800

    Create SysUser.cs

---
+++ Dg.ERM/Trump.Dg/Trump/Fonour.Domain/SysUserRoles/SysUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Fonour.Domain.SysUserRoles
+{
+    public class SysUser
+    {
+        public long ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+
+    public class SysRole
+    {
+        public long ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+
+    public class SysUserRole
+    {
+        public long ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
