9bbe57685201d484f23e96217b7041ca2aa56499
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-05-19 10:53:10 +0800

    Ids4

---
+++ Dg.ERM/PowerDg.Core.ERM/IdentityServerCenter/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api","My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client()
+                {
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new   Secret("secret".Sha256())
+                    },
+                    AllowedScopes={"api"}
+
+                }
+            };
+        }
+
+
+    }
+}
---
+++ Dg.ERM/PowerDg.Core.ERM/IdentityServerCenter/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using IdentityServer4;
+
+namespace IdentityServerCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetResources())
+                .AddInMemoryClients(Config.GetClients());
+            //http://localhost:50954/.well-known/openid-configuration
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+             
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //2918.05,18
+            //app.UseMvc();
+            //http://localhost:5000/.well-known/openid-configuration
+            app.UseIdentityServer();
+        }
+    }
+}
